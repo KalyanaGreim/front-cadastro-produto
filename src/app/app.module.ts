b30eb744c2a78c83b40b627f413f573c44506bf6
KalyanaGreim <kalygreim11@gmail.com>
2023-05-29 19:59:28 -0300

    Cria Front consumindo api de produtos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule} from '@angular/forms';
+import { ProdutoComponent } from './produto/produto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/produto.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Produto } from '../model/produto';
+import { API_CONFIG } from '../config/api.config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutoService {
+
+  constructor(private http: HttpClient) { }
+
+  findAll(): Observable<Produto[]> {
+    return this.http.get<Produto[]>(`${API_CONFIG.baseUrl}/produtos`);
+  }
+
+  findById(produto:Produto):Observable<Produto> {
+    return this.http.get<Produto>(`${API_CONFIG.baseUrl}/produtos/${produto.id}`);
+  }
+
+  save(produto:Produto):Observable<Produto> {
+    return this.http.post<Produto>(`${API_CONFIG.baseUrl}/produtos`, produto);
+  }
+  update(produto:Produto):Observable<Produto> {
+    return this.http.put<Produto>(`${API_CONFIG.baseUrl}/produtos`, produto);
+  }
+  delete(produto:Produto):Observable<void> {
+    return this.http.delete<void>(`${API_CONFIG.baseUrl}/produtos/${produto.id}`);
+  }
+}
